94c067b47697369ce8e808a9a7563f0aa3d5c2d3
Piotr Pawełczak <pecio@student.agh.edu.pl>
2022-05-12 18:49:43 +0200

    Style load file button

--- bpmn_web/settings.py
+++ bpmn_web/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-
 ]
 
 MIDDLEWARE = [
@@ -128,5 +126,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
